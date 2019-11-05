9b6c50c695a7867d65bc5c03008fda8338df3c98
Chris Haley <haleychristopher@cityuniversity.edu>
2019-11-04 21:48:00 -0800

    Dropdown on main works along with navigation between all current sites

--- KMCsportingGoods/KMC/sportingGoods/inventory/urls.py
+++ KMCsportingGoods/KMC/sportingGoods/inventory/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+#Delete when products urls gets completely built out
+from django.views.generic import TemplateView
+
 app_name = 'inventory_ns'
 
 urlpatterns = [
-    url(r'^$', views.add_product, name='products'),
+    url(r'^$', TemplateView.as_view(template_name='products.html')),
     url(r'^add_product/', views.add_product, name="add_product"),
 ]
